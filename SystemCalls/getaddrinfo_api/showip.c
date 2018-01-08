27f74de740df881fb8e07f6746976c0cf3b97b32
zhihui cao <zhcaoulaval@gmail.com>
2018-01-08 10:00:27 -0500

    Chapter System Call and Bust code walk

---
+++ SystemCalls/getaddrinfo_api/showip.c
@@ -0,0 +1,61 @@
+/*=====================
+	showip.c ---show IP address for a host given on the command line
+	use the getaddrinfo() api 
+========================*/
+
+#include<stdio.h>
+#include<string.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<netdb.h>
+#include<arpa/inet.h>
+#include<netinet/in.h>
+
+int main(int argc, char *argv[])
+{
+    struct addrinfo hints, *res, *p;
+	int status;	
+	char ipstr[INET6_ADDRSTRLEN];
+	if (argc != 2) 
+	{
+	    fprintf(stderr, "usage: showip hostname\n");
+	    return 1;
+	}
+    //zero the struct addrinfo hints before use it in getaddrinfo()
+    memset(&hints,0,sizeof(hints));
+    //setup some field of hints first 
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+
+    if ((status = getaddrinfo(argv[1],NULL, &hints,&res))!=0)
+    {
+        fprintf(stderr,"getaddrinfo:%s\n",gai_strerror(status));
+        return 2;
+    }    
+
+    printf("IP address for %s:\n\n",argv[1]);
+
+    for(p = res; p != NULL; p = p->ai_next)
+    {
+        void *addr;
+        void *ipver;
+        if (p->ai_family == AF_INET)
+        {
+            struct sockaddr_in *ipv4 = (struct sockaddr_in *) p->ai_addr;
+            addr = &(ipv4->sin_addr);
+            ipver = "IPv4";
+
+        }
+        else
+        {
+            struct sockaddr_in6 *ipv6 = (struct sockaddr_in6 *) p->ai_addr;
+            addr = &(ipv6->sin6_addr);
+            ipver = "IPv6";
+        }
+        inet_ntop(p->ai_family, addr,ipstr,sizeof(ipstr));
+        printf(" %s:%s\n",ipver, ipstr);
+    }
+    freeaddrinfo(res);
+    return 0;
+
+}
